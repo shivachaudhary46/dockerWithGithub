dc0a26cc120df430eb4e9cefba981b6374dfb1c5
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-06-21 09:07:00 +0545

    hello world

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-
-@app.route("/submit")
-def submit():
-    pass 
-
-
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
--- test_app.py
+++ test_app.py
@@ -4,8 +4,4 @@ def test_home():
     response=app.test_client().get("/")
 
     assert response.status_code==200
-    assert b"<h1>Hello World</h1>" in response.data
-
-
-    
-   
+    assert response.data==b"Hello World"
